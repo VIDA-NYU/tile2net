761b5614fcf68625702428af1f929c0d76725ea3
dhodcz2 <dhodcz2@uic.edu>
2024-03-13 15:21:49 -0500

    revert example.sh

--- examples/example.sh
+++ examples/example.sh
@@ -6,6 +6,5 @@ read output_dir
 echo "Tile generation will now begin."
 
 # Run the generate and inference commands
-#location="42.35555189953313, -71.07168915322092, 42.35364837213307, -71.06437423368418"
-location='Washington Square Park, New York, NY'
+location="42.35555189953313, -71.07168915322092, 42.35364837213307, -71.06437423368418"
 python -m tile2net generate -l "$location" -o "$output_dir" -n example | python -m tile2net inference
