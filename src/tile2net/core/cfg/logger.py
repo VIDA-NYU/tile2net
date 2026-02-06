5fdf00de34e58a56facd301062a95e00ec9bb4a1
Daniel Hodczak <dhodcz2@uic.edu>
2026-02-06 01:42:10 -0600

    for now, just set both loggers to debug, but later, we must have only one logger module

--- src/tile2net/core/cfg/logger.py
+++ src/tile2net/core/cfg/logger.py
@@ -7,8 +7,8 @@ fname = os.path.join(
     os.path.dirname(__file__),
     'logging.conf',
 )
-logging.config.fileConfig(fname)
-logger = logging.getLogger('user')
+logging.config.fileConfig(fname, disable_existing_loggers=False)
+logger = logging.getLogger('debug')
 logger.setLevel(getattr(logging, cfg.log_level))
 for h in logger.handlers:
     h.setLevel(getattr(logging, cfg.log_level))
--- src/tile2net/logger.py
+++ src/tile2net/logger.py
@@ -1,9 +1,10 @@
 import logging.config
 import os
 
+
 fname = os.path.join(
     os.path.dirname(__file__),
     'logging.conf',
 )
-logging.config.fileConfig(fname)
+logging.config.fileConfig(fname, disable_existing_loggers=False)
 logger = logging.getLogger('debug')
